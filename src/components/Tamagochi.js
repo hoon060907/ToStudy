054f62d0fbf41f70f3b5d42376e5929c617f7fc0
gihoon <gihoon0907@gmail.com>
2023-09-10 16:44:21 +0900

    렌더링 시 화면이 깨지는 문제 해결

--- src/components/Tamagochi.js
+++ src/components/Tamagochi.js
@@ -1,29 +1,24 @@
-import { useEffect, useRef, useState } from "react"
-
 const Tamagochi = ({ level }) => {
-    // const [path, setPath] = useState("images/egg.png");
-    let path = useRef("images/1.png");
     if(level > 60){
-        path.current="images/7.png";
+        return <img src="images/7.png" style={{ height: `${150 + 5*(level-1)}px` }} />;
     }
     else if(level > 50){
-        path.current="images/6.png";
+        return <img src="images/6.png" style={{ height: `${150 + 5*(level-1)}px` }} />;
     }
     else if(level > 40){
-        path.current="images/5.png";
+        return <img src="images/5.png" style={{ height: `${150 + 5*(level-1)}px` }} />;
     }
     else if(level > 30){
-        path.current="images/4.png";
+        return <img src="images/4.png" style={{ height: `${150 + 5*(level-1)}px` }} />;
     }
     else if(level > 20){
-        path.current="images/3.png";
+        return <img src="images/3.png" style={{ height: `${150 + 5*(level-1)}px` }} />;
     }
     else if(level > 10){
-        path.current="images/2.png";
+        return <img src="images/2.png" style={{ height: `${150 + 5*(level-1)}px` }} />;
     }
     else{
-        path.current="images/1.png";
+        return <img src="images/1.png" style={{ height: `${150 + 5*(level-1)}px` }} />;
     }
-    return <img src={path.current} style={{ width: `${150 + 5*(level-1)}px` }} />;
 }
 export default Tamagochi;
